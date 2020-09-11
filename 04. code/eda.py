5fcfd23bd66fd3db2ccd2bce1c23567cac14bd85
Jorge Saavedra <jorgeh.saavedra@gmovilsas.com.co>
2020-09-11 18:48:51 -0500

    Creacion de codigo para transformar los datos de entrada

---
+++ 04. code/eda.py
@@ -0,0 +1,19 @@
+#**********************************************************************************************
+# @Nombre: Analisis Exploratorio de datos
+# @Autor: Grupo 21
+# @Fecha: 2020/09/11 12:38:15
+# @Ayuda: 
+#**********************************************************************************************
+
+#Importar librerias ---------------------------------------------------------------------------
+import datetime as dt                 # Manipulacion de fechas
+import pandas as pd                   # Manipulacion de datos
+import numpy as np                    # Manipulacion numerica
+import keyring                        # Manejo de contraseñas
+
+#Desarrollo -----------------------------------------------------------------------------------
+
+
+
+
+
